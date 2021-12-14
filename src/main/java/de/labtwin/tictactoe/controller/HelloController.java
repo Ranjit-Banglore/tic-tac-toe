681cf1c8a0f0f8a02e689a92fbd147468ea0eaf5
ranjit <ranjit@denario.io>
2021-12-14 21:58:24 +0100

    checking scm polling

--- src/main/java/de/labtwin/tictactoe/controller/HelloController.java
+++ src/main/java/de/labtwin/tictactoe/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "hello world checking scm polling";
+        return "hello world checking scm polling-1";
     }
 }
